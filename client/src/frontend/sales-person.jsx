da3e978a35fabd5b28adf2cad51e726786fed1a6
Nitesh Chauhan <72297535+Nitesh121-maker@users.noreply.github.com>
2024-05-23 17:39:55 +0530

    Commited on 23 at 5:39

--- client/src/frontend/sales-person.jsx
+++ client/src/frontend/sales-person.jsx
@@ -7,7 +7,24 @@ const Salesperson = ({salespersonClient}) => {
     const[clientchat,setclientchat] = useState('');
     const sales_unique_id = salespersonClient.unique_id;
     const [selectedClientId, setSelectedClientId] = useState(null);
-
+    const[inprogress,setInprogress] = useState(true);
+    const[successful,setSuccessful] = useState(false);
+    const[closed,setClosed] = useState(false);
+    const handleinprogress =()=>{
+        setInprogress(true);
+        setSuccessful(false);
+        setClosed(false);
+    }
+    const handleSuccessful = ()=>{
+        setInprogress(false);
+        setSuccessful(true);
+        setClosed(false);
+    }
+    const handleClosed = ()=>{
+        setInprogress(false);
+        setSuccessful(false);
+        setClosed(true);
+    }
     useEffect(() => {
         const getClient = async () => {
             if (!sales_unique_id) return;
@@ -85,77 +102,165 @@ const Salesperson = ({salespersonClient}) => {
     console.log('Client',clientlist);
     console.log('Client Chat',clientchat);
   return (
-    <div className="row">
-        <div className="d-flex col-md-12">
-            <div className="card col-sm-12 col-md-4">
-                <div className="card-header text-white ">
-                     <div className=" justify-content-center">
-                        <h5 className="card-title">Client List</h5>
-                     </div>
-                </div>
-                <div className="list mt-4 ">
-                    {
-                       Array.isArray(clientlist) && clientlist.length ?(
-                           clientlist.map((clientlist) => (
-                                <div className="list-group ">
-                                    <li className='list-group-item list-bg text-white' onClick={() => handleClientClick(clientlist.unique_id)}>{clientlist.fullname}</li>
-                                </div>
-                           ))
-                       ):(
-                        <div className="text-center">No Client</div>
-                      )
+    <><div className="row">
+          <div className="d-flex col-md-12">
+              <div className="card col-sm-12 col-md-4">
+                  <div className="card-header text-white ">
+                      <div className=" justify-content-center">
+                          <h5 className="card-title">Client List</h5>
+                      </div>
+                  </div>
+                  <div className="list mt-4 ">
+                      {Array.isArray(clientlist) && clientlist.length ? (
+                          clientlist.map((clientlist) => (
+                              <div className="list-group ">
+                                  <li className='list-group-item list-bg text-white' onClick={() => handleClientClick(clientlist.unique_id)}>{clientlist.fullname}</li>
+                              </div>
+                          ))
+                      ) : (
+                          <div className="text-center">No Client</div>
+                      )}
+                      {/*
 
-                    }
-{/* 
+     <div className="list-group">
+        <li className='list-group-item list-bg text-white'>Shakti Raghav</li>
+     </div>
+     <div className="list-group">
+        <li className='list-group-item list-bg text-white'>Luckey Cipy</li>
+     </div>
+     <div className="list-group ">
+        <li className='list-group-item list-bg text-white'>Kundan Sharma</li>
+     </div> */}
+                  </div>
+              </div>
 
-                    <div className="list-group">
-                       <li className='list-group-item list-bg text-white'>Shakti Raghav</li>
-                    </div>
-                    <div className="list-group">
-                       <li className='list-group-item list-bg text-white'>Luckey Cipy</li>
-                    </div>
-                    <div className="list-group ">
-                       <li className='list-group-item list-bg text-white'>Kundan Sharma</li>
-                    </div> */}
-                </div>
-            </div>
-            <div className="card text-white col-sm-12 col-md-8">
-                <div className="card-header">
-                    Chat
-                </div>
-                <div className="d-flex card text-white chat-bg chat-card chat-style">
-                    {
-                        Array.isArray(clientchat) && clientchat.length ?(
-                            clientchat.map((clientchat) => (
-                                <>
-                                {clientchat.message&&
-                                <div className="card-body text-start col-md-10 sales-chat-bg flex-none" style={{ flex: 'none !important' }}>
-                                    <p>{clientchat.message}</p>
-                                    <span className='message-time'>{new Date(clientchat.sent_at).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
-                                </div>
-                                }
-                                {clientchat.adminmessage&&
-                                <div className="card-body text-end col-md-10 ml-auto admin-chat-bg flex-none">
-                                    <p>{clientchat.adminmessage}</p>
-                                    <span className='message-time'>11:42</span>
-                                </div>
-                                }
+              <div className="card text-white col-sm-12 col-md-8">
+                  <div className="card-header">
+                      Chat
+                  </div>
+                  <div className="d-flex card text-white chat-bg chat-card chat-style">
+                      {Array.isArray(clientchat) && clientchat.length ? (
+                          clientchat.map((clientchat) => (
+                              <>
+                                  {clientchat.message &&
+                                      <div className="card-body text-start col-md-10 sales-chat-bg flex-none" style={{ flex: 'none !important' }}>
+                                          <p>{clientchat.message}</p>
+                                          <span className='message-time'>{new Date(clientchat.sent_at).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
+                                      </div>}
+                                  {clientchat.adminmessage &&
+                                      <div className="card-body text-end col-md-10 ml-auto admin-chat-bg flex-none">
+                                          <p>{clientchat.adminmessage}</p>
+                                          <span className='message-time'>11:42</span>
+                                      </div>}
                               </>
-                            ))
-                        ):(
-                            <div className="text-center">No Chat</div>
-                        )
-                    }
-                </div>
-                <div className="card-footer">
-                     <form action="" className='d-flex' onSubmit={handleSendMessage}>
-                        <textarea name="message" value={adminmessage.message} onChange={handleAdminMessagechange} id=""  rows="1" className='form-control'  ></textarea>
-                        <button className="btn btn-primary"><FaPaperPlane/></button>
-                     </form>
+                          ))
+                      ) : (
+                          <div className="text-center">No Chat</div>
+                      )}
+                  </div>
+                  <div className="card-footer">
+                      <form action="" className='d-flex' onSubmit={handleSendMessage}>
+                          <textarea name="message" value={adminmessage.message} onChange={handleAdminMessagechange} id="" rows="1" className='form-control'></textarea>
+                          <button className="btn btn-primary"><FaPaperPlane /></button>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div className="row mt-4">
+            <div className="collg-12 col-md-12">
+                <div className="card">
+                    <div className="card-header">
+                        <h4 className="card-title text-white ml-3">Sales</h4>
+                    </div>
+                    <div className="card-body">
+                        <div className="card-header">
+                            <ul className='d-flex text-white button-list text-uppercase'>
+                                <li className='btn-rounded btn-fixed-w mb-3 mr-3 btn btn-outline-primary' onClick={handleinprogress}>In-Progress</li>
+                                <li className='btn-rounded btn-fixed-w mb-3 mr-3 btn btn-outline-success' onClick={handleSuccessful}>Successful</li>
+                                <li className='btn-rounded btn-fixed-w mb-3 mr-3 btn btn-outline-danger' onClick={handleClosed}>Closed</li>
+                            </ul>
+                        </div>
+                        {inprogress&&
+                            <div className="inprogress single-table">
+                                <div className="responsive">
+                                    <table className="table table-hover text-white progress-table text-center table">
+                                        <thead className="thead-light text-uppercase">
+                                            <tr>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Phone</th>
+                                                <th scope='col'>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className='text-uppercase'>
+                                            <tr>
+                                                <td>Shakti Raghav</td>
+                                                <td>info.Shakti@gmail.com</td>
+                                                <td>8789779098</td>
+                                                <td><button className='btn-rounded btn-fixed-w mb-3 mr-2 btn btn-outline-primary'>status</button></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        }
+                        {successful&&
+                            <div className="successful single-table">
+                                <div className="responsive">
+                                    <table className="table table-hover text-white progress-table text-center table">
+                                        <thead className="thead-light text-uppercase">
+                                            <tr>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Phone</th>
+                                                <th scope='col'>Invoice</th>
+                                                {/* <th scope='col'>Action</th> */}
+                                            </tr>
+                                        </thead>
+                                        <tbody className='text-uppercase'>
+                                            <tr>
+                                                <td>Nitesh Chauhan</td>
+                                                <td>info.nitesh@gmail.com</td>
+                                                <td>8789779098</td>
+                                                <td>TI/24-25/230</td>
+                                                {/* <td><button className='btn-rounded btn-fixed-w mb-3 mr-2 btn btn-outline-primary'>status</button></td> */}
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        }
+                        {closed&&
+                            <div className="closed single-table">
+                                <div className="responsive">
+                                    <table className="table table-hover text-white progress-table text-center table">
+                                        <thead className="thead-light text-uppercase">
+                                            <tr>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Phone</th>
+                                                <th scope='col'>Reason</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className='text-uppercase'>
+                                            <tr>
+                                                <td>Lucky Sippy</td>
+                                                <td>info.lucky@gmail.com</td>
+                                                <td>8789779098</td>
+                                                <td>Not Intrested</td>
+                                                {/* <td><button className='btn-rounded btn-fixed-w mb-3 mr-2 btn btn-outline-primary'>status</button></td> */}
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+      </div>
+    </>
   )
 }
 
